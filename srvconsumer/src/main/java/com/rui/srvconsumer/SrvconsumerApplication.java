854b6108d8b2a75f4e7cf2bbd9c9ac248c5a593b
liuweirui <liuweirui@outlook.com>
2021-03-20 11:36:39 +0800

    Eureka 集群测试

---
+++ srvconsumer/src/main/java/com/rui/srvconsumer/SrvconsumerApplication.java
@@ -0,0 +1,15 @@
+package com.rui.srvconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
+public class SrvconsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrvconsumerApplication.class, args);
+    }
+
+}
---
+++ srvconsumer/src/test/java/com/rui/srvconsumer/SrvconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rui.srvconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvconsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
